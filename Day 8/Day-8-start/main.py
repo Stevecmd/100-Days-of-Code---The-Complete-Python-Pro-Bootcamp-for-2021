26dc17535608d51c91ae5217886c85295891189e
Steve Murimi <72811315+Stevecmd@users.noreply.github.com>
2022-08-24 13:01:51 +0300

    Update main.py
    Use of functions with Keyword arguments.

--- Day 8/Day-8-start/main.py
+++ Day 8/Day-8-start/main.py
@@ -25,11 +25,26 @@
 #   print (f"Jambo {name}!")
 # greet_with_name()
 
+# Method - 3
 #Greeting with name and location
-♥
-#Method 3
-def greet(name, location):
-  print (f"\nHi {name}! You are from {location}")
-  print (f"Habari yako {name}?")
-  print (f"Jambo {name}!")
-greet("Jack", "Nairobi")
+# name=input("\nWhats your name please? ")
+# location = input("\nWhere are you from? ")
+# def greet_with_name():
+#   print (f"\nHi {name}! You are from {location}")
+#   print (f"Habari yako {name}?")
+#   print (f"Jambo {name}!")
+# greet_with_name()
+
+#Method 4 - Function allows for input
+# def greet(name, location):
+#   print (f"\nHi {name}! You are from {location}")
+#   print (f"Habari yako {name}?")
+#   print (f"Jambo {name}!")
+# greet("Jack", "Nairobi")
+
+#Method 5 - Functions with keyword arguments
+def greet_with(name, location):
+   print (f"\nHi {name}! You are from {location}")
+   print (f"Habari yako {name}?")
+   print (f"Jambo {name}!")
+ greet(location="Nairobi",name="Jack")    
