5a647f3f00784ceee590560f935276bacdb78a50
Steve Murimi <72811315+Stevecmd@users.noreply.github.com>
2022-08-24 12:41:20 +0300

    Update main.py
    Updated the greeting function with a more interactive iteration.

--- Day 8/Day-8-start/main.py
+++ Day 8/Day-8-start/main.py
@@ -3,10 +3,24 @@
 # Write 3 print statements inside the function.
 # Call the greet() function and run your code.
 
+#Method 1
+#def greet(name):
+#    print(f"Hi {name}" + "\n")
+#    print(f"Hey {name}" + "\n")
+#    print(f"Hello {name}" + "\n")
 
-def greet(name):
-    print(f"Hi {name}" + "\n")
-    print(f"Hey {name}" + "\n")
-    print(f"Hello {name}" + "\n")
+#greet("Solomon")
 
-greet("Solomon")
+#Method 2
+def greet():
+  print ("Hi")
+  print ("Habari")
+  print ("Jambo")
+greet()
+
+name=input("\nWhats your name please? ")
+def greet_with_name():
+  print (f"\nHi {name}!")
+  print (f"Habari yako {name}?")
+  print (f"Jambo {name}!")
+greet_with_name()
