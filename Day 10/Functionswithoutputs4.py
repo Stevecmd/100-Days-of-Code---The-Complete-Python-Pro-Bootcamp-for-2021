f9210f465d9e18c04b3753835267898a044702fb
Steve Murimi <72811315+Stevecmd@users.noreply.github.com>
2022-09-25 10:45:18 +0300

    Updating the code.
    Code update.

---
+++ Day 10/Functionswithoutputs4.py
@@ -0,0 +1,15 @@
+#Functions with outputs
+
+
+def format_name(f_name, l_name):
+    if f_name == '' or l_name == '':
+        return "You didn't provide valid inputs"
+    formated_f_name = f_name.title()
+    formated_l_name = l_name.title()
+    return (f"{formated_f_name}, {formated_l_name}")
+    return f_name
+
+
+print(
+    format_name(input("What is your first name? "),
+                input("What is your last name? ")))
--- Day 10/Leap_year2.py
+++ Day 10/Leap_year2.py
@@ -15,27 +15,30 @@ def is_leap(year):
         return False
 
 
-months = "";
-smonth = "";
+months = ""
+fmonth = ""
+
 
 def days_in_month(year, month):
     if year < 1:
-      return "Invalid year"
+        return "Invalid year"
     if month > 12 or month < 1:
-      return "Invalid month"
+        return "Invalid month"
+    """
+      Below is the array of days for each month as per the months array in order.       Does not account for Leap years 
+      """
     month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-    months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    months = [
+        "January", "February", "March", "April", "May", "June", "July",
+        "August", "September", "October", "November", "December"
+    ]
+# Accounting for the leap year for the month of February
     if is_leap(year) and month == 2:
         return 29
-    smonth = (months[month-1])
+    fmonth = (months[month - 1])
     # print(smonth)
-    print(f"You picked the month of: {smonth}, in the year {year}.")
+    print(f"You picked the month of: {fmonth}, in the year {year}.")
     return month_days[month - 1]
-    
-
-
-
-
 
 
 #🚨 Do NOT change any of the code below
@@ -44,6 +47,5 @@ month = int(input("Enter a month: "))
 days = days_in_month(year, month)
 print(f"It has {days} days")
 
-
-# print(smonth)
-# print(f"You picked the year: {year} and the {smonth} has {days} days.")
+# print(fmonth)
+# print(f"You picked the year: {year} and the {fmonth} has {days} days.")
