44cfa8d933999507ce819ce40b3020ba5dcfce70
Steve Murimi <72811315+Stevecmd@users.noreply.github.com>
2022-09-25 14:08:30 +0300

    Update recursiveCalculator.py
    Made some improvements, app is now recursive, no bugs so far. Accommodates  float input.

--- Day 10/recursiveCalculator.py
+++ Day 10/recursiveCalculator.py
@@ -1,3 +1,4 @@
+from art import logo
 #Calculator
 
 
@@ -19,15 +20,15 @@ def divide(n1, n2):
 
 def power(n1):
     # n2 = n1
-    return n1*n1
+    return n1 * n1
 
 
 operations = {"+": add, "-": subtract, "*": multiply, "/": divide, "**": power}
 
 
 def calculator():
-
-    n1 = int(input("What's the first number?: "))
+    print(logo)
+    n1 = float(input("What's the first number?: "))
     for symbol in operations:
         print(symbol)
     should_continue = True
@@ -35,22 +36,24 @@ def calculator():
     while should_continue:
         operation_symbol = input("Pick an operation: ")
         if operation_symbol == "**":
-          calculation_function = operations[operation_symbol]
-          answer = calculation_function(n1)
-          print(f"{n1} {operation_symbol} {n1} = {answer}")
-        else:  
-          n2 = int(input("Whats the next number?: "))
-          calculation_function = operations[operation_symbol]
-          answer = calculation_function(n1, n2)
-          print(f"{n1} {operation_symbol} {n2} = {answer}")
-  
+            calculation_function = operations[operation_symbol]
+            answer = calculation_function(n1)
+            print(f"{n1} {operation_symbol} {n1} = {answer}")
+        else:
+            n2 = float(input("Whats the next number?: "))
+            calculation_function = operations[operation_symbol]
+            answer = calculation_function(n1, n2)
+            print(f"{n1} {operation_symbol} {n2} = {answer}")
+
         if input(
-                f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: "
+                f"Type 'y' to continue calculating with {answer}, type 'n' to start a new calculation: "
         ) == "Y" or "y" or "Yes" or "yes":
             n1 = answer
         else:
-            should_continue = False
-            calculator()
+           should_continue = False
+        print("\nGoodbye!");
+        quit()
+               # calculator()
 
 
 calculator()
