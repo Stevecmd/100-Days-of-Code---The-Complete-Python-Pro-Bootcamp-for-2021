d39b8dd35730e1c2a4f0eaa07da5a0826eca2da7
Steve Murimi <72811315+Stevecmd@users.noreply.github.com>
2022-09-25 12:03:42 +0300

    Update Functionswithoutputs4.py
    Refactoring.

--- Day 10/Functionswithoutputs4.py
+++ Day 10/Functionswithoutputs4.py
@@ -6,8 +6,8 @@ def format_name(f_name, l_name):
         return "You didn't provide valid inputs"
     formated_f_name = f_name.title()
     formated_l_name = l_name.title()
-    return (f"{formated_f_name}, {formated_l_name}")
-    return f_name
+    return f"{formated_f_name}, {formated_l_name}"
+    
 
 
 print(
